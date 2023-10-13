02c5910eb43d28600db35187fbad94fa2615f009
UnethicalBear <78081711+UnethicalBear@users.noreply.github.com>
2023-10-13 10:27:47 +0100

    added RAM reading and updated init

--- Gecko_Main/Gecko_Main.cpp
+++ Gecko_Main/Gecko_Main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <string>
-#include <any>
 #include <vector>
 #include <fstream>
-#include <map>
+#include <stdexcept>
+#include <math.h>
 
 int main(){
     std::cout << "Hello World!\n";
 }
 
 class Gecko {
+private:
     int __config_RAM_word_width;
     int __config_RAM_SIZE_MAX;
     int __config_operandBits;
@@ -34,30 +35,45 @@ public:
         this->__config_alwaysUseBinary = alwaysUseBinary;
         this->__config_cacheMemorySize = cacheMemorySize;
         this->__config_cacheMemoryWidth = cacheMemoryWordWidth;
+
+        this->__config_opcodeBits = opcodeBits;
+        this->__config_operandBits = RAMwordSize - opcodeBits;
     }
 
     void readRAM(std::string inputFile) {
+        int word = 8;
         std::ifstream fileHandler(inputFile);
         std::string words;
-        while(inFile >> words) {
-            #ifdef GECKO_DEBUG
-            std::cout << words << std::endl;
-            #endif
+        char ch;
+        std::string tmpWORD = "";
+        int ctr = 0;
+        while (fileHandler.get(ch)){
+            tmpWORD += ch;
+            ctr++;
+            if (ctr == word){
+            try {
+                this->__internal_RAM.push_back(stoi(tmpWORD));
+            }
+            catch(...){
+                #ifdef DEBUG
+                std::cout << "Invalid";
+                #endif
+            }
+            ctr=0;
+            tmpWORD = "";
+            }
         }
-        for (auto word : words){
-            std::cout << word << std::endl;
+        #ifdef GECKO_DEBUG
+        for(int x : RAM){
+            int y = x / pow(10,word-4);
+            int z = x % (int)pow(10,word-4);
+            std::cout << y << " " << z << std::endl;
         }
+        #endif
     }
 
     virtual void interpretOpcodeOperandPair(int opcode, int operand) = 0;   
     // this turns gecko into an abstract class so it cannot be directly instantiated.
-
-
-    
-    
-
-
-
 
     ~Gecko() {
         
