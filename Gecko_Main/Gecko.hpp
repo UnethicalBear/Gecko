da4462ac2f7ba39c3049854c10c6c8091fc25144
UnethicalBear <shawleonm@gmail.com>
2023-10-14 09:53:15 +0100

    Add new methods, rename files
    1) Renamed to Gecko.cpp instead of Geko_Main
    
    2) Added boolean returns from functions to check if they ran ok
    
    3) Add new definitions for binary conversion (signed ints)
    
    4) Add vitual method for setting registers, added new method for quick run
    
    5) Updated example

---
+++ Gecko_Main/Gecko.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <math.h>
+
+// ----------------------- Basic Setup ------------------------------------- //
+
+#define GECKO_APP
+//#define GECKO_APP_DEBUG // put this in your file to turn DEBUG on.
+
+// ----------------------- Register Config ---------------------------------- //
+
+#define GECKO_STATUS_REG_ACC_IS_EVEN							1
+#define GECKO_STATUS_REG_ACC_IS_ODD								2
+#define GECKO_STATUS_REG_ACC_IS_ZERO							3
+#define GECKO_STATUS_REG_ACC_IS_NON_ZERO						4
+#define GECKO_STATUS_REG_ACC_PROCESSOR_OK						5
+
+// ------------------------ Arithmetic -------------------------------------- //
+
+#define GECKO_ARITHMETIC_CARRY									6
+#define GECKO_ARITHMETIC_BORROW									7		// } these are also registers
+
+#define GECKO_USE_SIGNED_INTS									0
+#define GECKO_USE_UNSIGNED_INTS									1
+
+
+// ----------------------- Integer to Binary Config ------------------------- //
+
+#define GECKO_BINSTR_CHRn                                       0
+#define GECKO_BINSTR_STDSTRING                                  1
+
+// ----------------------- String to Integer Exception Config --------------- //
+
+#define GECKO_APP_INVALID_CODE_TERMINATE						0
+#define GECKO_APP_INVALID_CODE_ADD_NULL							1
+#define GECKO_APP_INVALID_CODE_CUSTOM							2
+
+
+// ------------------------ Gecko Class Defenition Template ----------------- //
+
+// for some reason uncommenting this breaks EVERYTHING
+
+//class Gecko {
+//protected:
+//	std::string toBinaryString(int input, bool isSigned);
+//	const char* toBinaryArray(int input, bool isSigned);
+//
+//	int readCache(int cacheAddr);
+//	std::string readCacheBinaryString(int cacheAddr);
+//	char* readCacheBinaryArray(int cacheAddr);
+//
+//	void writeCache(int cacheAddr, int newValue);
+//	void writeCache(int cacheAddr, char* newValue);
+//	void writeCache(int cacheAddr, std::string newValue);
+//
+//public:
+//	Gecko();
+//	Gecko(int RAMAddrSize, int RAMwordSize, int opcodeBits, int cacheMemorySize, int cacheMemoryWordWidth, bool alwaysUseBinary = false);
+//
+//	void readRAM(std::string inputFile);
+//
+//	void execute();
+//	virtual void interpretOpcodeOperandPair(int opcode, int operand) = 0;
+//};
